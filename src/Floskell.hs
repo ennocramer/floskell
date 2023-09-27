ennocramer/floskell

commit 2d0763c29701398ceb60dc56a7bb5d78d9522863

Use Text instead of ByteString for all internal representations

src/Floskell.hs
@@ -24,29 +24,28 @@ module Floskell
     , defaultExtensions
     ) where
 
-import           Data.ByteString.Lazy       ( ByteString )
-import qualified Data.ByteString.Lazy.Char8 as L8
-import qualified Data.ByteString.Lazy.UTF8  as UTF8
 import           Data.List
 import           Data.Maybe
 #if __GLASGOW_HASKELL__ <= 802
 import           Data.Monoid
 #endif
+import           Data.Text.Lazy        ( Text )
+import qualified Data.Text.Lazy        as TL
 
-import qualified Floskell.Buffer            as Buffer
+import qualified Floskell.Buffer       as Buffer
 import           Floskell.Comments
 import           Floskell.Config
 import           Floskell.ConfigFile
-import           Floskell.Fixities          ( builtinFixities )
-import           Floskell.Pretty            ( pretty )
-import           Floskell.Styles            ( Style(..), styles )
+import           Floskell.Fixities     ( builtinFixities )
+import           Floskell.Pretty       ( pretty )
+import           Floskell.Styles       ( Style(..), styles )
 import           Floskell.Types
 
 import           Language.Haskell.Exts
                  hiding ( Comment, Pretty, Style, parse, prettyPrint, style )
-import qualified Language.Haskell.Exts      as Exts
+import qualified Language.Haskell.Exts as Exts
 
-data CodeBlock = HaskellSource Int [ByteString] | CPPDirectives [ByteString]
+data CodeBlock = HaskellSource Int [Text] | CPPDirectives [Text]
     deriving ( Show, Eq )
 
 trimBy :: (a -> Bool) -> [a] -> ([a], [a], [a])
@@ -60,63 +59,54 @@ trimBy f xs = (prefix, middle, suffix)
 
     suffix = reverse suffix'
 
-findLinePrefix :: (Char -> Bool) -> [ByteString] -> ByteString
+findLinePrefix :: (Char -> Bool) -> [Text] -> Text
 findLinePrefix _ [] = ""
-findLinePrefix f (x : xs') = go (L8.takeWhile f x) xs'
+findLinePrefix f (x : xs') = go (TL.takeWhile f x) xs'
   where
-    go prefix xs = if all (prefix `L8.isPrefixOf`) xs
+    go prefix xs = if all (prefix `TL.isPrefixOf`) xs
                    then prefix
-                   else go (L8.take (L8.length prefix - 1) prefix) xs
+                   else go (TL.take (TL.length prefix - 1) prefix) xs
 
-findIndent :: (Char -> Bool) -> [ByteString] -> ByteString
+findIndent :: (Char -> Bool) -> [Text] -> Text
 findIndent _ [] = ""
-findIndent f (x : xs') = go (L8.takeWhile f x) $ filter (not . L8.all f) xs'
+findIndent f (x : xs') = go (TL.takeWhile f x) $ filter (not . TL.all f) xs'
   where
-    go indent xs = if all (indent `L8.isPrefixOf`) xs
+    go indent xs = if all (indent `TL.isPrefixOf`) xs
                    then indent
-                   else go (L8.take (L8.length indent - 1) indent) xs
+                   else go (TL.take (TL.length indent - 1) indent) xs
 
-preserveVSpace :: Monad m
-               => ([ByteString] -> m [ByteString])
-               -> [ByteString]
-               -> m [ByteString]
+preserveVSpace :: Monad m => ([Text] -> m [Text]) -> [Text] -> m [Text]
 preserveVSpace format input = do
     output <- format input'
     return $ prefix ++ output ++ suffix
   where
-    (prefix, input', suffix) = trimBy L8.null input
+    (prefix, input', suffix) = trimBy TL.null input
 
-preservePrefix :: Monad m
-               => (Int -> [ByteString] -> m [ByteString])
-               -> [ByteString]
-               -> m [ByteString]
+preservePrefix :: Monad m => (Int -> [Text] -> m [Text]) -> [Text] -> m [Text]
 preservePrefix format input = do
     output <- format (prefixLength prefix) input'
     return $ map (prefix <>) output
   where
     prefix = findLinePrefix allowed input
 
-    input' = map (L8.drop $ L8.length prefix) input
+    input' = map (TL.drop $ TL.length prefix) input
 
     allowed c = c == ' ' || c == '\t' || c == '>'
 
-    prefixLength = sum . map (\c -> if c == '\t' then 8 else 1) . L8.unpack
+    prefixLength = sum . map (\c -> if c == '\t' then 8 else 1) . TL.unpack
 
-preserveIndent :: Monad m
-               => (Int -> [ByteString] -> m [ByteString])
-               -> [ByteString]
-               -> m [ByteString]
+preserveIndent :: Monad m => (Int -> [Text] -> m [Text]) -> [Text] -> m [Text]
 preserveIndent format input = do
     output <- format (prefixLength prefix) input'
     return $ map (prefix <>) output
   where
     prefix = findIndent allowed input
 
-    input' = map (L8.drop $ L8.length prefix) input
+    input' = map (TL.drop $ TL.length prefix) input
 
     allowed c = c == ' ' || c == '\t'
 
-    prefixLength = sum . map (\c -> if c == '\t' then 8 else 1) . L8.unpack
+    prefixLength = sum . map (\c -> if c == '\t' then 8 else 1) . TL.unpack
 
 withReducedLineLength :: Int -> Config -> Config
 withReducedLineLength offset config = config { cfgPenalty = penalty }
@@ -127,13 +117,11 @@ withReducedLineLength offset config = config { cfgPenalty = penalty }
                                   }
 
 -- | Format the given source.
-reformat
-    :: AppConfig -> Maybe FilePath -> ByteString -> Either String ByteString
-reformat config mfilepath input = fmap (L8.intercalate "\n")
-    . preserveVSpace (preservePrefix (reformatLines mode cfg)) $
-    L8.split '\n' input
+reformat :: AppConfig -> Maybe FilePath -> Text -> Either String Text
+reformat config mfilepath input = fmap (TL.intercalate "\n")
+    . preserveVSpace (preservePrefix (reformatLines mode cfg)) $ TL.lines input
   where
-    mode = case readExtensions $ UTF8.toString input of
+    mode = case readExtensions $ TL.unpack input of
         Nothing -> mode'
         Just (Nothing, exts') ->
             mode' { extensions = exts' ++ extensions mode' }
@@ -151,8 +139,7 @@ reformat config mfilepath input = fmap (L8.intercalate "\n")
 
     cfg = safeConfig . styleConfig $ appStyle config
 
-reformatLines
-    :: ParseMode -> Config -> Int -> [ByteString] -> Either String [ByteString]
+reformatLines :: ParseMode -> Config -> Int -> [Text] -> Either String [Text]
 reformatLines mode config indent = format . filterPreprocessorDirectives
   where
     config' = withReducedLineLength indent config
@@ -166,8 +153,8 @@ reformatBlock :: ParseMode
               -> Config
               -> [Comment]
               -> Int
-              -> [ByteString]
-              -> Either String [ByteString]
+              -> [Text]
+              -> Either String [Text]
 reformatBlock mode config cpp indent lines =
     case parseModuleWithComments mode code of
         ParseOk (m, comments') ->
@@ -176,11 +163,11 @@ reformatBlock mode config cpp indent lines =
             in
                 case prettyPrint (pretty ast) config' of
                     Nothing -> Left "Printer failed with mzero call."
-                    Just output -> Right $ L8.lines output
+                    Just output -> Right $ TL.lines output
         ParseFailed loc e -> Left $
             Exts.prettyPrint (loc { srcLine = srcLine loc }) ++ ": " ++ e
   where
-    code = UTF8.toString $ L8.intercalate "\n" lines
+    code = TL.unpack $ TL.intercalate "\n" lines
 
     config' = withReducedLineLength indent config
 
@@ -195,7 +182,7 @@ reformatBlock mode config cpp indent lines =
         else y : mergeComments xs ys'
 
 -- | Remove CPP directives from input source, retur
-filterPreprocessorDirectives :: [ByteString] -> ([ByteString], [Comment])
+filterPreprocessorDirectives :: [Text] -> ([Text], [Comment])
 filterPreprocessorDirectives lines = (code, comments)
   where
     code = map (\l -> if cppLine l then "" else l) lines
@@ -204,11 +191,11 @@ filterPreprocessorDirectives lines = (code, comments)
 
     makeComment (n, l) =
         Comment PreprocessorDirective
-                (SrcSpan "" n 1 n (fromIntegral $ L8.length l + 1))
-                (L8.unpack l)
+                (SrcSpan "" n 1 n (fromIntegral $ TL.length l + 1))
+                (TL.unpack l)
 
     cppLine src =
-        any (`L8.isPrefixOf` src)
+        any (`TL.isPrefixOf` src)
             [ "#if"
             , "#end"
             , "#else"
@@ -220,8 +207,8 @@ filterPreprocessorDirectives lines = (code, comments)
             , "#warning"
             ]
 
-prettyPrint :: Printer a -> Config -> Maybe ByteString
-prettyPrint printer = fmap (Buffer.toLazyByteString . psBuffer . snd)
+prettyPrint :: Printer a -> Config -> Maybe Text
+prettyPrint printer = fmap (Buffer.toLazyText . psBuffer . snd)
     . execPrinter printer . initialPrintState
 
 -- | Default extensions.

src/Floskell/Buffer.hs
@@ -1,5 +1,5 @@
--- | An outout buffer for ByteStrings that keeps track of line and
--- column numbers.
+-- | An outout buffer for Text that keeps track of line and column
+-- numbers.
 module Floskell.Buffer
     ( Buffer
     , empty
@@ -7,13 +7,14 @@ module Floskell.Buffer
     , write
     , line
     , column
-    , toLazyByteString
+    , toLazyText
     ) where
 
-import qualified Data.ByteString         as BS
-import           Data.ByteString.Builder ( Builder )
-import qualified Data.ByteString.Builder as BB
-import qualified Data.ByteString.Lazy    as BL
+import           Data.Text              ( Text )
+import qualified Data.Text              as T
+import qualified Data.Text.Lazy         as TL
+import           Data.Text.Lazy.Builder ( Builder )
+import qualified Data.Text.Lazy.Builder as TB
 
 data Buffer =
     Buffer { bufferData        :: !Builder -- ^ The current output.
@@ -30,17 +31,17 @@ empty = Buffer { bufferData        = mempty
                , bufferColumn      = 0
                }
 
--- | Append a ByteString to the output buffer.  It is an error for the
+-- | Append a Text to the output buffer.  It is an error for the
 -- string to contain newlines.
-write :: BS.ByteString -> Buffer -> Buffer
+write :: Text -> Buffer -> Buffer
 write str buf =
     buf { bufferData        = newBufferData
         , bufferDataNoSpace =
-              if BS.all (== 32) str then bufferData buf else newBufferData
-        , bufferColumn      = bufferColumn buf + BS.length str
+              if T.all (== ' ') str then bufferData buf else newBufferData
+        , bufferColumn      = bufferColumn buf + T.length str
         }
   where
-    newBufferData = bufferData buf `mappend` BB.byteString str
+    newBufferData = bufferData buf `mappend` TB.fromText str
 
 -- | Append a newline to the output buffer.
 newline :: Buffer -> Buffer
@@ -50,7 +51,7 @@ newline buf = buf { bufferData        = newBufferData
                   , bufferColumn      = 0
                   }
   where
-    newBufferData = bufferDataNoSpace buf `mappend` BB.char7 '\n'
+    newBufferData = bufferDataNoSpace buf `mappend` TB.singleton '\n'
 
 -- | Return the current line number, counting from 0.
 line :: Buffer -> Int
@@ -60,6 +61,6 @@ line = bufferLine
 column :: Buffer -> Int
 column = bufferColumn
 
--- | Return the contents of the output buffer as a lazy ByteString.
-toLazyByteString :: Buffer -> BL.ByteString
-toLazyByteString = BB.toLazyByteString . bufferData
+-- | Return the contents of the output buffer as a lazy Text.
+toLazyText :: Buffer -> TL.Text
+toLazyText = TB.toLazyText . bufferData

src/Floskell/Config.hs
@@ -35,18 +35,17 @@ module Floskell.Config
 
 import           Data.Aeson
                  ( FromJSON(..), ToJSON(..), genericParseJSON, genericToJSON )
-import qualified Data.Aeson         as JSON
-import           Data.Aeson.Types   as JSON
+import qualified Data.Aeson        as JSON
+import           Data.Aeson.Types  as JSON
                  ( Options(..), camelTo2, typeMismatch )
-import           Data.ByteString    ( ByteString )
-import           Data.Default       ( Default(..) )
-import qualified Data.HashMap.Lazy  as HashMap
-import           Data.Map.Strict    ( Map )
-import qualified Data.Map.Strict    as Map
-import           Data.Set           ( Set )
-import qualified Data.Set           as Set
-import qualified Data.Text          as T
-import qualified Data.Text.Encoding as T ( decodeUtf8, encodeUtf8 )
+import           Data.Default      ( Default(..) )
+import qualified Data.HashMap.Lazy as HashMap
+import           Data.Map.Strict   ( Map )
+import qualified Data.Map.Strict   as Map
+import           Data.Set          ( Set )
+import qualified Data.Set          as Set
+import           Data.Text         ( Text )
+import qualified Data.Text         as T
 
 import           GHC.Generics
 
@@ -71,7 +70,7 @@ data Whitespace = Whitespace { wsSpaces         :: !WsLoc
 data Layout = Flex | Vertical | TryOneline
     deriving ( Eq, Ord, Bounded, Enum, Show, Generic )
 
-data ConfigMapKey = ConfigMapKey !(Maybe ByteString) !(Maybe LayoutContext)
+data ConfigMapKey = ConfigMapKey !(Maybe Text) !(Maybe LayoutContext)
     deriving ( Eq, Ord, Show )
 
 data ConfigMap a =
@@ -310,7 +309,7 @@ safeConfig cfg = cfg { cfgGroup = group, cfgOp = op }
                        (cfgMapFind ctx key config) { wsSpaces = ws }
                        m
 
-cfgMapFind :: LayoutContext -> ByteString -> ConfigMap a -> a
+cfgMapFind :: LayoutContext -> Text -> ConfigMap a -> a
 cfgMapFind ctx key ConfigMap{..} =
     let value = cfgMapDefault
         value' = Map.findWithDefault value
@@ -325,10 +324,10 @@ cfgMapFind ctx key ConfigMap{..} =
     in
         value'''
 
-cfgOpWs :: LayoutContext -> ByteString -> OpConfig -> Whitespace
+cfgOpWs :: LayoutContext -> Text -> OpConfig -> Whitespace
 cfgOpWs ctx op = cfgMapFind ctx op . unOpConfig
 
-cfgGroupWs :: LayoutContext -> ByteString -> GroupConfig -> Whitespace
+cfgGroupWs :: LayoutContext -> Text -> GroupConfig -> Whitespace
 cfgGroupWs ctx op = cfgMapFind ctx op . unGroupConfig
 
 inWs :: Location -> WsLoc -> Bool
@@ -418,20 +417,19 @@ textToLayout _ = Nothing
 
 keyToText :: ConfigMapKey -> T.Text
 keyToText (ConfigMapKey Nothing Nothing) = "default"
-keyToText (ConfigMapKey (Just n) Nothing) = T.decodeUtf8 n
+keyToText (ConfigMapKey (Just n) Nothing) = n
 keyToText (ConfigMapKey Nothing (Just l)) = "* in " `T.append` layoutToText l
 keyToText (ConfigMapKey (Just n) (Just l)) =
-    T.decodeUtf8 n `T.append` " in " `T.append` layoutToText l
+    n `T.append` " in " `T.append` layoutToText l
 
 textToKey :: T.Text -> Maybe ConfigMapKey
 textToKey t = case T.splitOn " in " t of
     [ "default" ] -> Just (ConfigMapKey Nothing Nothing)
     [ "*", "*" ] -> Just (ConfigMapKey Nothing Nothing)
-    [ name ] -> Just (ConfigMapKey (Just (T.encodeUtf8 name)) Nothing)
-    [ name, "*" ] -> Just (ConfigMapKey (Just (T.encodeUtf8 name)) Nothing)
+    [ name ] -> Just (ConfigMapKey (Just name) Nothing)
+    [ name, "*" ] -> Just (ConfigMapKey (Just name) Nothing)
     [ "*", layout ] -> ConfigMapKey Nothing . Just <$> textToLayout layout
-    [ name, layout ] -> ConfigMapKey (Just (T.encodeUtf8 name)) . Just
-        <$> textToLayout layout
+    [ name, layout ] -> ConfigMapKey (Just name) . Just <$> textToLayout layout
     _ -> Nothing
 
 instance ToJSON a => ToJSON (ConfigMap a) where

src/Floskell/Pretty.hs
@@ -12,13 +12,12 @@ import           Control.Monad
 import           Control.Monad.State.Strict   ( get, gets, modify )
 
 import           Data.Bool                    ( bool )
-import           Data.ByteString              ( ByteString )
-import qualified Data.ByteString              as BS
-import qualified Data.ByteString.Char8        as BS8
-import qualified Data.ByteString.Lazy         as BL
 import           Data.List                    ( groupBy, sortBy, sortOn )
 import           Data.Maybe                   ( catMaybes, fromMaybe )
 import qualified Data.Set                     as Set
+import           Data.Text                    ( Text )
+import qualified Data.Text                    as T
+import qualified Data.Text.Lazy               as TL
 
 import qualified Floskell.Buffer              as Buffer
 import           Floskell.Config
@@ -154,14 +153,14 @@ printComment correction (Comment{..}, nextSpan) = do
             column 0 $ string commentText
             modify (\s -> s { psEolComment = True })
         InlineComment -> do
-            write $ BS.replicate padding 32
+            write $ T.replicate padding " "
             write "{-"
             string commentText
             write "-}"
             when (srcSpanEndLine commentSpan /= srcSpanStartLine nextSpan) $
                 modify (\s -> s { psEolComment = True })
         LineComment -> do
-            write $ BS.replicate padding 32
+            write $ T.replicate padding " "
             write "--"
             string commentText
             modify (\s -> s { psEolComment = True })
@@ -205,13 +204,13 @@ printCommentsInternal nlBefore loc ast = unless (null comments) $ do
         < srcSpanStartLine (commentSpan comment)
 
 -- | Return the configuration name of an operator
-opName :: QOp a -> ByteString
+opName :: QOp a -> Text
 opName op = case op of
     (QVarOp _ qname) -> opName' qname
     (QConOp _ qname) -> opName' qname
 
 -- | Return the configuration name of an operator
-opName' :: QName a -> ByteString
+opName' :: QName a -> Text
 opName' (Qual _ _ name) = opName'' name
 opName' (UnQual _ name) = opName'' name
 opName' (Special _ (FunCon _)) = "->"
@@ -219,9 +218,9 @@ opName' (Special _ (Cons _)) = ":"
 opName' (Special _ _) = ""
 
 -- | Return the configuration name of an operator
-opName'' :: Name a -> ByteString
+opName'' :: Name a -> Text
 opName'' (Ident _ _) = "``"
-opName'' (Symbol _ str) = BS8.pack str
+opName'' (Symbol _ str) = T.pack str
 
 lineDelta :: Annotated ast => ast NodeInfo -> ast NodeInfo -> Int
 lineDelta prev next = nextLine - prevLine
@@ -262,16 +261,16 @@ lined = linedFn pretty
 linedOnside :: (Annotated ast, Pretty ast) => [ast NodeInfo] -> Printer ()
 linedOnside = linedFn prettyOnside
 
-listVOpLen :: LayoutContext -> ByteString -> Printer Int
+listVOpLen :: LayoutContext -> Text -> Printer Int
 listVOpLen ctx sep = do
     ws <- getConfig (cfgOpWs ctx sep . cfgOp)
     return $ if wsLinebreak After ws
              then 0
-             else BS.length sep + if wsSpace After ws then 1 else 0
+             else T.length sep + if wsSpace After ws then 1 else 0
 
 listVinternal :: (Annotated ast, Pretty ast)
               => LayoutContext
-              -> ByteString
+              -> Text
               -> [ast NodeInfo]
               -> Printer ()
 listVinternal ctx sep xs = case xs of
@@ -296,9 +295,9 @@ listVinternal ctx sep xs = case xs of
 
 listH :: (Annotated ast, Pretty ast)
       => LayoutContext
-      -> ByteString
-      -> ByteString
-      -> ByteString
+      -> Text
+      -> Text
+      -> Text
       -> [ast NodeInfo]
       -> Printer ()
 listH _ open close _ [] = do
@@ -310,9 +309,9 @@ listH ctx open close sep xs =
 
 listV :: (Annotated ast, Pretty ast)
       => LayoutContext
-      -> ByteString
-      -> ByteString
-      -> ByteString
+      -> Text
+      -> Text
+      -> Text
       -> [ast NodeInfo]
       -> Printer ()
 listV ctx open close sep xs = groupV ctx open close $ do
@@ -325,9 +324,9 @@ listV ctx open close sep xs = groupV ctx open close $ do
 
 list :: (Annotated ast, Pretty ast)
      => LayoutContext
-     -> ByteString
-     -> ByteString
-     -> ByteString
+     -> Text
+     -> Text
+     -> Text
      -> [ast NodeInfo]
      -> Printer ()
 list ctx open close sep xs = oneline hor <|> ver
@@ -338,14 +337,14 @@ list ctx open close sep xs = oneline hor <|> ver
 
 listH' :: (Annotated ast, Pretty ast)
        => LayoutContext
-       -> ByteString
+       -> Text
        -> [ast NodeInfo]
        -> Printer ()
 listH' ctx sep = inter (operatorH ctx sep) . map pretty
 
 listV' :: (Annotated ast, Pretty ast)
        => LayoutContext
-       -> ByteString
+       -> Text
        -> [ast NodeInfo]
        -> Printer ()
 listV' ctx sep xs =
@@ -353,7 +352,7 @@ listV' ctx sep xs =
 
 list' :: (Annotated ast, Pretty ast)
       => LayoutContext
-      -> ByteString
+      -> Text
       -> [ast NodeInfo]
       -> Printer ()
 list' ctx sep xs = oneline hor <|> ver
@@ -364,9 +363,9 @@ list' ctx sep xs = oneline hor <|> ver
 
 listAutoWrap :: (Annotated ast, Pretty ast)
              => LayoutContext
-             -> ByteString
-             -> ByteString
-             -> ByteString
+             -> Text
+             -> Text
+             -> Text
              -> [ast NodeInfo]
              -> Printer ()
 listAutoWrap _ open close _ [] = do
@@ -378,7 +377,7 @@ listAutoWrap ctx open close sep xs =
 
 listAutoWrap' :: (Annotated ast, Pretty ast)
               => LayoutContext
-              -> ByteString
+              -> Text
               -> [ast NodeInfo]
               -> Printer ()
 listAutoWrap' _ _ [] = return ()
@@ -386,7 +385,7 @@ listAutoWrap' ctx sep (x : xs) = aligned $ do
     ws <- getConfig (cfgOpWs ctx sep . cfgOp)
     let correction = if wsLinebreak After ws
                      then 0
-                     else BS.length sep + if wsSpace After ws then 1 else 0
+                     else T.length sep + if wsSpace After ws then 1 else 0
     col <- getNextColumn
     pretty x
     go (col - correction) xs
@@ -412,7 +411,7 @@ measure p = do
     return $ case execPrinter (oneline p) s' of
         Nothing -> Nothing
         Just (_, s'') -> Just . (\x -> x - psIndentLevel s) . fromIntegral
-            . BL.length . Buffer.toLazyByteString $ psBuffer s''
+            . TL.length . Buffer.toLazyText $ psBuffer s''
 
 measure' :: Printer a -> Printer (Maybe [Int])
 measure' p = fmap (: []) <$> measure p
@@ -554,7 +553,7 @@ prettyDecls fn dc = inter sep . map lined . runs fn
 
 prettySimpleDecl :: (Annotated ast1, Pretty ast1, Annotated ast2, Pretty ast2)
                  => ast1 NodeInfo
-                 -> ByteString
+                 -> Text
                  -> ast2 NodeInfo
                  -> Printer ()
 prettySimpleDecl lhs op rhs = withLayout cfgLayoutDeclaration flex vertical
@@ -625,7 +624,7 @@ prettyTypesig ctx names ty = do
         nl <- gets psNewline
         when nl $ do
             delta <- listVOpLen ctx "->"
-            write $ BS.replicate delta 32
+            write $ T.replicate delta " "
         pretty ty
 
 prettyApp :: (Annotated ast1, Annotated ast2, Pretty ast1, Pretty ast2)
@@ -646,7 +645,7 @@ prettyApp fn args = withLayout cfgLayoutApp flex vertical
 
 prettyInfixApp
     :: (Annotated ast, Pretty ast, Annotated op, HSE.Pretty (op NodeInfo))
-    => (op NodeInfo -> ByteString)
+    => (op NodeInfo -> Text)
     -> LayoutContext
     -> (ast NodeInfo, [(op NodeInfo, ast NodeInfo)])
     -> Printer ()
@@ -702,10 +701,10 @@ prettyRecordFields len ctx fields = withLayout cfgLayoutRecord flex vertical
                             (fmap (fmap pure) . len)
                             fields $ listVinternal ctx "," fields
 
-prettyPragma :: ByteString -> Printer () -> Printer ()
+prettyPragma :: Text -> Printer () -> Printer ()
 prettyPragma name = prettyPragma' name . Just
 
-prettyPragma' :: ByteString -> Maybe (Printer ()) -> Printer ()
+prettyPragma' :: Text -> Maybe (Printer ()) -> Printer ()
 prettyPragma' name mp = do
     write "{-# "
     write name
@@ -1616,17 +1615,17 @@ instance Pretty Exp where
             write $ if nl && alignP then "in  " else "in "
             prettyOnside expr
 
-        vertical = withIndentAfter
-                       cfgIndentLet
-                       (do
-                            write "let"
-                            withIndent cfgIndentLetBinds $
-                                pretty (CompactBinds binds))
-                       (do
-                            newline
-                            alignP <- getOption cfgOptionAlignLetBindsAndInExpr
-                            write $ if alignP then "in " else "in"
-                            withIndent cfgIndentLetIn $ pretty expr)
+        vertical =
+            withIndentAfter cfgIndentLet
+                            (do
+                                 write "let"
+                                 withIndent cfgIndentLetBinds $
+                                     pretty (CompactBinds binds))
+                            (do
+                                 newline
+                                 alignP <- getOption cfgOptionAlignLetBindsAndInExpr
+                                 write $ if alignP then "in " else "in"
+                                 withIndent cfgIndentLetIn $ pretty expr)
 
     prettyPrint (If _ expr expr' expr'') = withLayout cfgLayoutIf flex vertical
       where
@@ -2198,7 +2197,7 @@ instance Pretty ModulePragma where
         string (trim str)
       where
         name = case mtool of
-            Just tool -> "OPTIONS_" `mappend` BS8.pack (HSE.prettyPrint tool)
+            Just tool -> "OPTIONS_" `mappend` T.pack (HSE.prettyPrint tool)
             Nothing -> "OPTIONS"
 
         trim = reverse . dropWhile (== ' ') . reverse . dropWhile (== ' ')

commit dcc009314cf26296f7ddf03fa4bcd454ee14dd65

Fix bug swallowing final newline introduced in 2d0763c

src/Floskell.hs
@@ -119,7 +119,8 @@ withReducedLineLength offset config = config { cfgPenalty = penalty }
 -- | Format the given source.
 reformat :: AppConfig -> Maybe FilePath -> Text -> Either String Text
 reformat config mfilepath input = fmap (TL.intercalate "\n")
-    . preserveVSpace (preservePrefix (reformatLines mode cfg)) $ TL.lines input
+    . preserveVSpace (preservePrefix (reformatLines mode cfg)) $
+    TL.split (== '\n') input
   where
     mode = case readExtensions $ TL.unpack input of
         Nothing -> mode'